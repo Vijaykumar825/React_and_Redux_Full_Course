a0f205040a37503189b0038f916e75f081d6f563
Vijay229 <Vijay229>
2025-04-23 12:33:51 +0530

    Fragments and syntax of fragments

---
+++ 18Fragments/Learning-Fragments/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+
+function App() {
+  return (
+    /*<React.Fragment>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </React.Fragment>*/
+    //or down one both gives the same ans
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;

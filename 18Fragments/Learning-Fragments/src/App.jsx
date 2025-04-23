f3c617496548299127b6bfb9bb7c75756b2b57e3
Vijay229 <Vijay229>
2025-04-23 13:05:37 +0530

    Using Map Method

--- 18Fragments/Learning-Fragments/src/App.jsx
+++ 18Fragments/Learning-Fragments/src/App.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
-function App() {
+/*function App() {
   return (
-    /*<React.Fragment>
+    <React.Fragment>
       <h1>Healthy Food</h1>
       <ul class="list-group">
         <li class="list-group-item">An item</li>
@@ -13,19 +13,35 @@ function App() {
         <li class="list-group-item">A fourth item</li>
         <li class="list-group-item">And a fifth one</li>
       </ul>
-    </React.Fragment>*/
+    </React.Fragment>
     //or down one both gives the same ans
     <>
       <h1>Healthy Food</h1>
       <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Green vegetables</li>
+        <li class="list-group-item">roti</li>
+        <li class="list-group-item">Egg</li>
+        <li class="list-group-item">Green leafs</li>
       </ul>
     </>
   );
 }
+*/
 
+function App() {
+  let foodItems = ["Dal", "Green Vegtables", "roti", "sald", "Egg", "Ghee"];
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul className="list-group">
+        {foodItems.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 export default App;

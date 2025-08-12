c5d48b77662f57087a4778c282f50e6e16713894
vijay <kumarvijay34453@gmail.com>
2025-08-12 16:37:58 +0530

    learned contextAPI

---
+++ 35_36_Context_API/src/componets/AddTodo.jsx
@@ -0,0 +1,53 @@
+import { useState, useContext } from "react";
+import { TodoItemsContext } from "../store/todo-items-store"; // Assuming the context is imported
+
+function AddTodo() {
+  // In the screenshot, onNewItem was called but addNewItem was defined from context.
+  // This version uses the context function, which is a common pattern.
+  const { addNewItem } = useContext(TodoItemsContext);
+  const [todoName, setTodoName] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = () => {
+    addNewItem(todoName, dueDate);
+    setDueDate("");
+    setTodoName("");
+  };
+
+  return (
+    <div className="container ">
+      <div className="row kg-row">
+        <div className="col-6">
+          <input
+            type="text"
+            placeholder="Enter Todo Here"
+            value={todoName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" value={dueDate} onChange={handleDateChange} />
+        </div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-success kg-button" // Using Bootstrap classes for styling
+            onClick={handleAddButtonClicked}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
